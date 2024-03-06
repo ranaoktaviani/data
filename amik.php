1c1515c2d3b6d65fff30677ef84aa190ee7984b6
aptnjordan <159239731+aptnjordan@users.noreply.github.com>
2024-03-06 15:12:10 +0700

    Update amik.php

--- amik.php
+++ amik.php
@@ -5,6 +5,8 @@
         echo "Nama : Aprianton Jordan";
         echo "Nim : 22110002";
         echo "Alamat : Garuda Sakti";
+        echo "Hobby : Playing Game";
+        echo "Jenis Kelamin : Laki-Laki";
 ?>
   </head>
 </html>

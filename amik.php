fd5cb2ecec275d9c2fe425be167c8c585956d17f
ranaoktaviani <160466637+ranaoktaviani@users.noreply.github.com>
2024-03-06 15:08:45 +0700

    Create amik.php
    Data Siswa

---
+++ amik.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Data Mahasiswa Amik</title>
+      <?php
+        echo "Nama : Aprianton Jordan";
+        echo "Nim : 22110002";
+        echo "Alamat : Garuda Sakti";
+?>
+  </head>
+</html>
